6aedae6846488441b070fc5ef5a8ac2c87c74c5e
azizbek400 <aziznormurodov400@gmail.com>
2023-12-09 23:17:38 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let bilet = 500;
+let mehmonxona = 250;
+let joylar = 120;
+let dollar = 11000.34;
+let yevro =  12354.03;
+dollor1 = bilet * dollar;
+dollor2 = mehmonxona * dollar;
+yevro1 = joylar * yevro;
+hammasi = dollor1 + dollor2 + yevro1;
+
+
+console.log(dollor1);
+console.log(dollor2);
+console.log(yevro1);
+console.log(hammasi);
+
+
+if (hammasi > 10000000) {
+    console.log("Oq yo'l, Alisher!");
+} 
+else{
+    console.log("Alisher, ozgina sabr qilish kerak bo'lar ekan.");
+}
